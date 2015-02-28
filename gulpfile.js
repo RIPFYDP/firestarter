6435e30fadd2630f8ecedf903b5063777c791eb4
Jason Kim <jasonkim@rhapsody.com>
2015-02-28 13:00:50 -0800

    Added readme and removed gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -35,48 +35,6 @@ gulp.task('nodemon', function() {
   }).on('restart');
 });
 
-gulp.task('db:drop', function() {
-  Mongo.connect(development.database.fullUrl, function(err, db) {
-    assert.equal(null, err);
-
-    db.dropDatabase(function(err, result) {
-      assert.equal(null, err);
-
-      // Wait to seconds to let it replicate across
-      setTimeout(function() {
-
-        db.close();
-      }, 2000);
-    });
-  });
-});
-
-gulp.task('db:seed', function() {
-  var webpages = [];
-
-  _.times(100, function() {
-    webpages.push({
-      body: faker.lorem.paragraph(),
-      url: faker.internet.ip(),
-      date: faker.date.recent()
-    });
-  });
-
-  Mongo.connect(development.database.fullUrl, function(err, db) {
-    assert.equal(null, err);
-
-    db.createCollection('webpages', {}, function(err, collection) {
-      assert.equal(null, err);
-
-      collection.insert(webpages, function(err, result) {
-        assert.equal(null, err);
-
-        db.close();
-      })
-    });
-  });
-});
-
 gulp.task('db:test:drop', function() {
   var deferred = Q.defer();
 

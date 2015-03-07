9a3db590ec460e9dca6bfc7c7536638e7ddf992a
Jason Kim <jasonkim@rhapsody.com>
2015-03-07 11:16:22 -0800

    Earlybird.insertOneQ

--- app/models/earlybird.js
+++ app/models/earlybird.js
@@ -2,10 +2,35 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var Q = require('q');
 var _ = require('lodash');
+var validator = require('validator');
+
+var validators = {
+  isEmail: function(val) {
+    return validator.isEmail(val);
+  }
+};
+
+var toValidate = {
+  url: [validators.isEmail, 'Please use a valid email.']
+};
 
 var earlybirdSchema = new Schema({
   date: { type: Date, default: Date.now },
+  email: { type: String, required: true, validate: toValidate.url, unique: true }
 });
 
 var Earlybird = mongoose.model('Earlybird', earlybirdSchema);
 module.exports = Earlybird;
+
+Earlybird.insertOneQ = function(data) {
+  var deferred = Q.defer();
+
+  Earlybird.create(data, function(err, result) {
+    if (err) {
+      return deferred.reject(err);
+    }
+    deferred.resolve(result);
+  });
+
+  return deferred.promise;
+};
--- test/app/models/earlybird_test.js
+++ test/app/models/earlybird_test.js
@@ -3,10 +3,11 @@ var expect = chai.expect;
 var Q = require('q');
 var _ = require('lodash');
 var app = require('../../../app');
+var faker = require('faker');
 
 var Earlybird = require('../../../app/models/earlybird');
 
-describe('entity model', function() {
+describe('earlybird model', function() {
 
   before(function(done) {
     app.main('test');
@@ -19,7 +20,19 @@ describe('entity model', function() {
     done();
   });
 
-  it('', function(done) {
+  it('.insertOneQ', function(done) {
+    var eb = { email: faker.internet.email() };
+
+    Earlybird.insertOneQ(eb)
+    .then(function(earlybird) {
+      expect(earlybird).to.be.a('object');
+      expect(earlybird.email).to.equal(eb.email);
+      done();
+    }, function(err) {
+      console.log('error:', err);
+      expect(err).to.equal(null);
+      done();
+    });
   });
 
 });

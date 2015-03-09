ed18c5c82b9835c99df7908231e765ade78b5403
Jason Kim <jasonkim@rhapsody.com>
2015-03-08 23:38:40 -0700

    checking to make sure fileLocation exists

--- config/attachments/environment.js
+++ config/attachments/environment.js
@@ -1,13 +1,14 @@
 var environment = {};
 var development = require('../environments/development');
 var test = require('../environments/test');
-var staging = require('../environment/staging');
+var staging = require('../environments/staging');
 var fs = require('fs');
 
 environment = {
   init: function(app) {
     environment.development = development;
     environment.test = test;
+    environment.staging = staging;
 
     switch (app.get('env')) {
       case 'development':
@@ -28,7 +29,15 @@ environment = {
   },
 
   setVariables: function() {
-    var variables = JSON.parse(fs.readFileSync('/etc/firestarter/firestarter.json', 'utf8'));
+    var variables = {};
+    var fileLocation = '/etc/firestarter/firestarter.json';
+
+    // Damn it, I like using #existsSync()
+    if (fs.existsSync(fileLocation)) {
+      var file = fs.readFileSync(fileLocation, 'utf8');
+      variables = JSON.parse(file);
+    }
+
     environment.current.variables = variables;
   }
 };

6d1a24fb3eaaaefe7e591012092eeb448450b7ac
Jason Kim <jasonkim@rhapsody.com>
2015-03-14 14:00:00 +0100

    verification association

--- app/models/verification.js
+++ app/models/verification.js
@@ -5,11 +5,9 @@ var _ = require('lodash');
 
 var verificationSchema = new Schema({
   date: { type: Date, default: Date.now },
-  trail: { type: String, required: true, unique: true }
+  trail: { type: String, required: true, unique: true },
+  _earlybird: { type: Schema.Types.ObjectId, ref: 'Earlybird' }
 });
 
 var Verification = mongoose.model('Verification', earlybirdSchema);
 module.exports = Verification;
-
-
-git commit --amend --date="Sat Mar 13 14:00 2015 +0100"

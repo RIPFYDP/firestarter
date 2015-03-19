3f9de23e140a9677d535efe2b1e8a12316a47809
Jason Kim <jasonkim@rhapsody.com>
2015-03-19 20:00:00 +0100

    Environment based variable

--- config/attachments/global.js
+++ config/attachments/global.js
@@ -1,6 +1,24 @@
 var fs = require('fs');
 var _ = require('lodash');
 
+var setHash = function(hash) {
+  _.each(hash, function(value, key) {
+    process.env[key] = value;
+  });
+};
+
+var developmentVariables = {
+  'APP_NAME': '6Compass',
+  'PROTOCOL': 'http',
+  'HOST': 'localhost:3000'
+};
+
+var stagingVariables = {
+  'APP_NAME': '6Compass',
+  'PROTOCOL': 'http',
+  'HOST': '104.236.91.35'
+};
+
 var global = {
   // TODO: change to async
   setEnvVariables: function() {
@@ -13,13 +31,13 @@ var global = {
       variables = JSON.parse(file);
     }
 
-    _.each(variables, function(value, key) {
-      process.env[key] = value;
-    });
+    setHash(variables);
 
-    process.env.APP_NAME = '6Compass';
-    process.env.PROTOCOL = 'http';
-    process.env.HOST = 'localhost:3000';
+    if (process.env.ENVIRONMENT === 'development') {
+      setHash(developmentVariables);
+    } else if (process.env.ENVIRONMENT === 'staging') {
+      setHash(stagingVariables);
+    }
   }
 };
 
